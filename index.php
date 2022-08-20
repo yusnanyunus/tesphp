2ee2c47f9a158178844ece570c496b330e9aa71b
Yusnan Yunus <yusnanyunus24@gmail.com>
2022-08-21 06:55:59 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Appointment Schedule Board | General Repair</title>
+<style>
+.container {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+  padding-top: 66.66%; /* 3:2 Aspect Ratio */
+}
+
+.responsive-iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+</style>
+<script type="text/javascript">
+</script>
+
+</head>
+<body>
+
+
+<div class="container">	
+<iframe src="https://dmsservice-dev.hkalla.co.id/appoitment-schedule-board/20202/<?php echo date('d/m/Y'); ?>" class="responsive-iframe" frameborder="0" allowfullscreen></iframe>
+</div>
+
+</body>
+
+</html>
